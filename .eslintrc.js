e40b79d989db60b180bd900b06e7566d16d2726f
Rui Jiang <rjiang.bb@gmail.com>
2018-04-30 09:33:51 -0400

    Add ESLint support.

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'extends': 'google',
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaVersion': 7,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'comma-dangle': 0,
+    'block-spacing': 0,
+    'require-jsdoc': 0
+  }
+};
